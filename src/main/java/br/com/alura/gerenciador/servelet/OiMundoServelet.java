04270c2490d8fba250be85c82f6bf6468dad3c30
Gabriel Ewerton <gabrielewerton62@gmail.com>
2022-09-10 15:07:29 -0300

    Primeiro commit

---
+++ src/main/java/br/com/alura/gerenciador/servelet/OiMundoServelet.java
@@ -0,0 +1,17 @@
+package br.com.alura.gerenciador.servelet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class OiMundoServelet extends HttpServlet{
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+	
+}
