835e774e437493f47d1688ea2662888e83b6db24
Gabriel Ewerton <gabrielewerton62@gmail.com>
2022-09-10 15:25:18 -0300

    Chamando um servlet

--- src/main/java/br/com/alura/gerenciador/servelet/OiMundoServelet.java
+++ src/main/java/br/com/alura/gerenciador/servelet/OiMundoServelet.java
@@ -1,16 +1,33 @@
 package br.com.alura.gerenciador.servelet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet(urlPatterns="/oi")
 public class OiMundoServelet extends HttpServlet{
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		PrintWriter out = resp.getWriter();
+		
+		out.println("<html>");
+		out.println("<body>");
+		out.println("oi mundo");
+		out.println("</body>");
+		out.println("</html>");
+		
+		
+		
+		
+		
+		
 		
 	}
 	
