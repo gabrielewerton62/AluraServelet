de9538a30ae843735dab3e3221deae2b4b58917a
Gabriel Ewerton <gabrielewerton62@gmail.com>
2022-09-10 18:14:32 -0300

    Despachando a requisição

--- src/main/java/br/com/alura/gerenciador/servelet/NovaEmpresaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servelet/NovaEmpresaServlet.java
@@ -3,6 +3,7 @@ package br.com.alura.gerenciador.servelet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -32,7 +33,10 @@ public class NovaEmpresaServlet extends HttpServlet {
 		Banco banco = new Banco();
 		banco.adiciona(empresa);
 		
-		out.println("<html><body>Cadastro effetivado: " + nomeEmpresa + "</body></html>");
+		RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
+		request.setAttribute("empresa", empresa.getNome());
+		rd.forward(request, response);
+		
 	
 	}
 
