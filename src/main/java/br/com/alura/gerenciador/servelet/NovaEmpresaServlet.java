71ce92a231f780181bb6536994b0981962c44384
Gabriel Ewerton <gabrielewerton62@gmail.com>
2022-09-10 17:27:09 -0300

    Listan empresas

--- src/main/java/br/com/alura/gerenciador/servelet/NovaEmpresaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servelet/NovaEmpresaServlet.java
@@ -17,7 +17,8 @@ public class NovaEmpresaServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
 		
 		System.out.println("Cadastro");
 		
