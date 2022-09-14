c8913ae0030611f87b6641de7d920f320aabe634
Gabriel Ewerton <gabrielewerton62@gmail.com>
2022-09-14 15:47:25 -0300

    Correção da nomenclatura servlet e adição da lista de empresas

--- src/main/java/br/com/alura/gerenciador/servlet/Empresa.java
+++ src/main/java/br/com/alura/gerenciador/servlet/Empresa.java
@@ -1,4 +1,4 @@
-package br.com.alura.gerenciador.servelet;
+package br.com.alura.gerenciador.servlet;
 
 public class Empresa {
 	
--- src/main/java/br/com/alura/gerenciador/servlet/ListaEmpresasServelet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/ListaEmpresasServelet.java
@@ -1,10 +1,11 @@
-package br.com.alura.gerenciador.servelet;
+package br.com.alura.gerenciador.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,17 +27,11 @@ public class ListaEmpresasServelet extends HttpServlet {
 		
 		List<Empresa> lista = banco.getEmpresas();
 		
-		PrintWriter out  = response.getWriter();
+		request.setAttribute("empresas", lista);
+
+		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas.jsp");
 		
-		out.println("<html><body>");
-		out.println("<ul>");
-		
-		for (Empresa empresa : lista) {
-			out.println("<li>" + empresa.getNome() + "</li>");
-		}
-		
-		out.println("</ul>");
-		out.println("</body></html>");
+		rd.forward(request, response);
 		
 		
 		
